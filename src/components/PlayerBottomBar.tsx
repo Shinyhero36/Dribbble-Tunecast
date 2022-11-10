6c584218637728db025f7909387fc6ebb89ca24c
Gérald LEBAN <gerald.leban0@gmail.com>
2022-11-10 15:02:19 +0100

    Change icon size

--- src/components/PlayerBottomBar.tsx
+++ src/components/PlayerBottomBar.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useState } from "react";
+import { Playback } from "../types/types";
 import {
   BackwardIcon,
   ForwardIcon,
@@ -10,7 +11,6 @@ import {
   SpeakerIcon,
   SpeakerMuteIcon,
 } from "./Icons";
-import { Playback } from "./PlayerCard";
 
 interface Props {
   playback: Playback;
@@ -73,28 +73,28 @@ const PlayerBottomBar = ({ playback }: Props) => {
         <div className="flex items-center justify-center gap-4">
           {/* Shuffle */}
           <Control className="hidden sm:block">
-            <ShuffleIcon className="h-5 w-5 text-gray-400 sm:h-6 sm:w-6" />
+            <ShuffleIcon className="h-6 w-6 text-gray-400" />
           </Control>
           {/* Previous - Play/Pause - Next */}
           <div className="flex items-center justify-between gap-1 pr-4 sm:pr-0">
             {/* Previous */}
             <Control>
-              <BackwardIcon className="h-6 w-6 text-gray-400 sm:h-8 sm:w-8" />
+              <BackwardIcon className="h-6 w-6 text-gray-400" />
             </Control>
 
             {/* Play/Pause */}
             <Control className="rounded-full bg-orange-500 p-1">
-              <PlayIcon className="h-6 w-6 text-white sm:h-8 sm:w-8" />
+              <PlayIcon className="h-6 w-6 text-white" />
             </Control>
 
             {/* Next */}
             <Control>
-              <ForwardIcon className="h-6 w-6 text-gray-400 sm:h-8 sm:w-8" />
+              <ForwardIcon className="h-6 w-6 text-gray-400" />
             </Control>
           </div>
           {/* Repeat */}
           <Control className="hidden sm:block">
-            <RepeatIcon className="h-5 w-5 text-gray-400 sm:h-6 sm:w-6" />
+            <RepeatIcon className="h-6 w-6 text-gray-400" />
           </Control>
         </div>
 
@@ -102,17 +102,17 @@ const PlayerBottomBar = ({ playback }: Props) => {
         <div className="hidden items-center justify-between gap-4 pr-10 sm:flex">
           <div className="items-centeer flex justify-center gap-2">
             <Control>
-              <HeartIcon className="h-6 w-6 text-gray-400 sm:h-8 sm:w-8" />
+              <HeartIcon className="h-6 w-6 text-gray-400" />
             </Control>
             <Control>
-              <PlusCircleIcon className="h-6 w-6 text-gray-400 sm:h-8 sm:w-8" />
+              <PlusCircleIcon className="h-6 w-6 text-gray-400" />
             </Control>
           </div>
           <div className="flex items-center justify-center gap-2">
             {volume === 0 ? (
-              <SpeakerMuteIcon className="h-6 w-6 text-gray-400 sm:h-8 sm:w-8" />
+              <SpeakerMuteIcon className="h-6 w-6 text-gray-400" />
             ) : (
-              <SpeakerIcon className="h-6 w-6 text-gray-400 sm:h-8 sm:w-8" />
+              <SpeakerIcon className="h-6 w-6 text-gray-400 sm:h-6" />
             )}
             <input
               type={"range"}
